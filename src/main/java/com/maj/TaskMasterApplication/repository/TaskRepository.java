1211a3cd73c591813d003185dad277c4a3d020de
alma.mancaku <alma.mancaku@gmail.con>
2025-05-17 11:06:03 +0200

    krijimi i model,repository,service

---
+++ src/main/java/com/maj/TaskMasterApplication/repository/TaskRepository.java
@@ -0,0 +1,18 @@
+package com.maj.TaskMasterApplication.repository;
+
+import com.maj.TaskMasterApplication.model.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public interface TaskRepository extends JpaRepository<Task, Long> {
+
+    List<Task> findByCompleted(boolean completed);
+
+    List<Task> findByTitleContainingIgnoreCase(String title);
+
+    List<Task> findByDueDateBefore(LocalDateTime dateTime);
+
+    List<Task> findByPriority(Task.Priority priority);
+}
